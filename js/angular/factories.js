831986a2dbbb963fd2a08344e70e2eaa1fafc56e
Adam Kelsall <adam.kelsall25@gmail.com>
2016-12-05 11:08:23 +0000

    Factories exercise solution (#15)

---
+++ js/angular/factories.js
@@ -0,0 +1,24 @@
+// Factory for storing and modifying what is in the basket
+moviesApp.factory('basketFactory', function () {
+
+  // Starting basket contents - empty
+  var movies = {};
+
+  return {
+
+    // List all movies stored in the basket
+    getMovies: function () {
+      return movies;
+    },
+
+    // Add a movie to the basket
+    addMovie: function (movie) {
+      movies[movie.id] = movie;
+    },
+
+    // Remove a movie from the basket
+    removeMovie: function (movie) {
+      delete movies[movie.id];
+    }
+  };
+});
